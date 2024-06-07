dc33983e5bae9c3c95a45be3f48268a4f31debfe
Darshan Badiger <dbadiger1997@gmail.com>
2024-06-07 10:19:19 +0530

    Custom React Modification in Vite

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,16 @@
 function App() {
+//Inject Variables in react
+const username = "Darshan"
+
+
   return (
+    <>
     <h1>Vite React App</h1>
+    <h2>Welcome {username}</h2>
+    {/* {username}  --> it is evaluated expression
+    (final outcome of the javascript is added here) 
+    return evaluation cannot be writtern*/}
+    </>
   )
 }
 export default App
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
+import {jsx as _jsx}from "react/jsx-runtime.js"
 import App from './App.jsx'
 
+
+function MyApp(){
+  return (
+    <div>Custom React App | DB</div>
+  )
+}
+/*
+const ReactElement={
+  type:'a',
+  props:{
+    href:'https://google.com',
+    target:'_blank'
+  },
+  Children:'Visit Now'
+}*/
+
+
+//Here how a variable is injected in React?
+const newvariable = "New Variable Injected"
+const reactElement = React.createElement(
+  'a',
+  {href:"https://google.com",target:'_blank'},
+  'click me to visit Google',
+  newvariable //Variable name is directly added.
+)
+
+const anotherElement = (
+  <a href='https://google.com'>Google</a>
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  // MyApp()
+    //  <MyApp />
+    // reactElement
+    // ReactElement
+    // anotherElement
+    reactElement
+  // </React.StrictMode>,
 )
