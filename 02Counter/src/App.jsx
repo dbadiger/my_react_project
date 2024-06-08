5feb5ebb0beb6871ff8862b8ae5bf7974d70c633
Darshan Badiger <dbadiger1997@gmail.com>
2024-06-08 12:37:30 +0530

    useState

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -13,6 +13,21 @@ function App() {
       alert("Counter reached maximum value")
     }else{
       setCounter(counter+1)
+      setCounter(counter+1)
+      setCounter(counter+1)
+      setCounter(counter+1)
+      //In the above four lines of code, we try to increment counter four times. But behid the secene, it will not increemnted four times.
+      //Because, in react, state update is asynchronous. All four functions bunched together as one, all are doining same work, so increment will not happen four times.
+
+      setCounter((prevCounter)=>prevCounter+1)  
+      setCounter((prevCounter)=>prevCounter+1)  
+      setCounter((prevCounter)=>prevCounter+1)  
+      /*Here, prevCounter will give us an existing State. setCounter function accepts a call back function and a variable(prevCounter: which is last updated State). 
+      PrevCounter will gives previously updated value, and in setCounter, one value will be incremented.So, we called setCounter function 3 times, value is incremented 3 times.
+      
+      We can alos write it as mention below */
+      setCounter(prevCounter=>prevCounter+1)
+
     }
   }
 
